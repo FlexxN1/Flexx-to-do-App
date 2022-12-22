5eae4504dff67db119570fb7e8dbe852615eea29
Flexx <juandavidx40@gmail.com>
2022-12-21 21:39:31 -0500

    Add updates

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lear React with platzi
         </a>
       </header>
     </div>
