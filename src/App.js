21c0c0e6689896d6069feecd0bc7879bc37cf4f9
Flexx <juandavidx40@gmail.com>
2022-12-22 14:47:39 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+
 import './App.css';
+
+const todos = [
+  {text: 'cortar cebolla', complete: false },
+  {text: 'Tomar el curso de intro a React', complete: false },
+  {text: 'Llorar con la llorona', complete: false }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lear React platzi
-        </a>
-      </header>
-    </div>
+    <> {/*tambien se puede hacer React.Fragment*/}
+      <TodoCounter />
+      <h2>has completado 2 de 3 todos</h2>
+      <TodoSearch />
+      
+      <TodoList>
+          {todos.map(todo => (
+            <TodoItem />
+          ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+      <button></button>
+    </>
   );
 }
 
