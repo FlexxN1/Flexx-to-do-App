6ccaa2d5b557d56b38a170d95565499332996628
Flexx <juandavidx40@gmail.com>
2023-02-02 15:32:44 -0500

    Update Files, remove ReactContext

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,65 +1,72 @@
-import React from "react";
+import React from 'react';
 import { TodoContext } from '../TodoContext';
+import { TodoHeader } from '../TodoHeader';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
-import { TodoForm } from '../TodoForm';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
-import { Modal } from '../Modal';
 import { TodosError } from '../TodosError';
 import { TodosLoading } from '../TodosLoading';
 import { EmptyTodos } from '../EmptyTodos';
+import { TodoForm } from '../TodoForm';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { Modal } from '../Modal';
 
-function AppUI(){
-    const {
-        error,
-        loading,
-        searchedTodos,
-        completeTodo,
-        deleteTodo,
-        openModal,
-        setOpenModal
-        } = React.useContext(TodoContext);
+function AppUI() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+  } = React.useContext(TodoContext);
+  
+  return (
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter
+          totalTodos={totalTodos}
+          completedTodos={completedTodos}
+        />
+        <TodoSearch
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </TodoHeader>
 
-    return (
-        <> {/*tambien se puede hacer React.Fragment*/}
-            <TodoCounter />
-            <TodoSearch />
-            
-            {/* Podemos acceder a nuestro contexto con el consumer */}
-            <TodoList>
-                {/* Mostramos un mensaje en caso de que ocurra algún error*/}
-                {error && <TodosError error={error} />}
-                {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando los datos */}
-                {loading && <TodosLoading />}
-                {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para crear el primer TODO */}
-                {(!loading && !searchedTodos.length) && <EmptyTodos />}
+      <TodoList>
+        {error && <TodosError />}
+        {loading && <TodosLoading />}
+        {(!loading && !searchedTodos.length) && <EmptyTodos />}
+        
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
 
-                {/* Regresamos solamente los TODOs buscados */}
-                {searchedTodos.map(todo => (
-                    <TodoItem 
-                    key={todo.text} 
-                    text={todo.text}
-                    completed={todo.completed}
-                    onComplete={() => completeTodo(todo.text)}
-                    onDelete={() => deleteTodo(todo.text)}
-                    />
-                ))}
-            </TodoList>
-            {!!openModal && (
-                <Modal>
-                    <TodoForm />
-                </Modal>
-            )}
+      {!!openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
 
-            <CreateTodoButton
-                setOpenModal={setOpenModal}
-                openModal={openModal}
-            />
-        </>
-    );
-};
-
+      <CreateTodoButton
+        setOpenModal={setOpenModal}
+      />
+    </React.Fragment>
+  );
+}
 
 export { AppUI };
